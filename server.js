93315081b1a5c66a4f520b2702bbf79a0f75acfc
anwarenrique <amendez@gwu.edu>
2023-05-20 11:13:45 -0400

    added landing page tailwind code

--- server.js
+++ server.js
@@ -3,48 +3,50 @@ const express = require("express");
 const app = express();
 const PORT = 8500;
 const mongoose = require("mongoose");
-const passport = require("passport")
-const session = require("express-session")
-const MongoStore = require('connect-mongo')
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 //*Import functions/routes
-const connectDB = require("./config/database")
-const homeRoutes = require("./routes/home")
-const editRoutes = require("./routes/edit")
+const connectDB = require("./config/database");
+const homeRoutes = require("./routes/home");
+const editRoutes = require("./routes/edit");
 
-require('dotenv').config({path: './config/.env'})
+require("dotenv").config({ path: "./config/.env" });
 
 //Pasport config
-require("./config/passport")(passport)
+require("./config/passport")(passport);
 
 //todo - Connect to Database
-connectDB()
+connectDB();
 
 //todo - Set Middleware
 app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //Sessions
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-        mongoUrl: process.env.DB_CONNECTION
-    })
-}))
+      mongoUrl: process.env.DB_CONNECTION,
+    }),
+  })
+);
 
 //Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //*Required to properly parse form POST requests - sending data
 app.use(express.urlencoded({ extended: true }));
 
 //todo - Set Routes
-app.use('/', homeRoutes)
-app.use('/edit', editRoutes)
-app.use('/auth', require('./routes/auth'))
+app.use("/", homeRoutes);
+app.use("/edit", editRoutes);
+app.use("/auth", require("./routes/auth"));
 
 //todo - Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
