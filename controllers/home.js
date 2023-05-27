e268b2c34edd719f5f27ca3b90acae620e53f032
anwarenrique <amendez@gwu.edu>
2023-05-27 18:09:35 -0400

    Added error pages, edited schema

--- controllers/home.js
+++ controllers/home.js
@@ -1,61 +1,56 @@
-const ItemList = require('../models/ItemList')
+const ItemList = require("../models/ItemList");
 
 module.exports = {
-    getLogin : async (req, res) => {
-        try {
-            const items = await
-            
-            res.render("login.ejs");
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-        }
-    },
-    getIndex : async (req, res) => {
-        try {
-            const items = await
-            ItemList.find()
-            res.render("index.ejs", { itemList: items });
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-        }
-    },
-    getDashboard : async (req, res) => {
-        try {
-            const items = await
-            ItemList.find()
-            res.render("dashboard.ejs", { itemList: items });
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-        }
-    },
-    createItem: async (req, res) => {
-        const newItem = new ItemList(
-            {
-                titleinput: req.body.titleinput,
-                textinput: req.body.textinput,
-                linkinput: req.body.linkinput,
-                numinput: req.body.numinput,
-                regioninput: req.body.regioninput,
-                countryinput: req.body.countryinput,
-                healthriskinput: req.body.healthriskinput,
-                citationinput: req.body.citationinput
-            });
-        try {
-            await newItem.save();
-            console.log(newItem)
-            res.redirect("/");
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-            res.redirect("/dashboard");
-        }
-    },
-    getGuestDashboard: async (req, res) => {
-        try {
-            const items = await
-            ItemList.find()
-            res.render("guestDashboard.ejs", { itemList: items });
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-        }
+  getLogin: async (req, res) => {
+    try {
+      const items = await res.render("login.ejs");
+    } catch (err) {
+      if (err) return res.status(500).send(err);
     }
-}
+  },
+  getIndex: async (req, res) => {
+    try {
+      const items = await ItemList.find();
+      res.render("index.ejs", { itemList: items });
+    } catch (err) {
+      if (err) return res.status(500).send(err);
+    }
+  },
+  getDashboard: async (req, res) => {
+    try {
+      const items = await ItemList.find();
+      res.render("dashboard.ejs", { itemList: items });
+    } catch (err) {
+      res.render("error/500");
+      if (err) return res.status(500).send(err);
+    }
+  },
+  createItem: async (req, res) => {
+    const newItem = new ItemList({
+      titleinput: req.body.titleinput,
+      textinput: req.body.textinput,
+      linkinput: req.body.linkinput,
+      numinput: req.body.numinput,
+      regioninput: req.body.regioninput,
+      countryinput: req.body.countryinput,
+      healthriskinput: req.body.healthriskinput,
+      citationinput: req.body.citationinput,
+    });
+    try {
+      await newItem.save();
+      console.log(newItem);
+      res.redirect("/");
+    } catch (err) {
+      if (err) return res.status(500).send(err);
+      res.redirect("/dashboard");
+    }
+  },
+  getGuestDashboard: async (req, res) => {
+    try {
+      const items = await ItemList.find();
+      res.render("guestDashboard.ejs", { itemList: items });
+    } catch (err) {
+      if (err) return res.status(500).send(err);
+    }
+  },
+};
--- models/itemlist.js
+++ models/itemlist.js
@@ -1,37 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const User = require("./User");
 const itemListSchema = new mongoose.Schema({
-titleinput: {
+  titleinput: {
     type: String,
-    required: true
-    },
-textinput: {
+    required: true,
+  },
+  textinput: {
     type: String,
-    required: true
-},
-linkinput: {
+    required: true,
+  },
+  linkinput: {
     type: String,
-    required: true
-},
-numinput: {
+    required: true,
+  },
+  numinput: {
     type: Number,
-    required: true
-},
-regioninput: {
-    type: String
-},
-countryinput: {
-    type: [String]
-    
-},
-healthriskinput: {
-    type: [String]
-},
-citationinput: {
-    type: String
-},
-date: {
+    required: true,
+  },
+  regioninput: {
+    type: String,
+  },
+  countryinput: {
+    type: [String],
+  },
+  healthriskinput: {
+    type: [String],
+  },
+  citationinput: {
+    type: String,
+  },
+  createdAt: {
     type: Date,
-    default: Date.now
-}
-})
-module.exports = mongoose.model('ItemList',itemListSchema,'items');
+    default: Date.now,
+  },
+  User: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  // postedBy: {
+  //   type: String,
+  //   ref: "User.displayName",
+  // },
+});
+module.exports = mongoose.model("ItemList", itemListSchema, "items");
